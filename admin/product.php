4c1f371303d501a1e6b869ffcf1826268788a86a
Samkay1974 <samuel.ninson@ashesi.edu.gh>
2025-10-25 22:48:02 +0000

    Update product.php

--- admin/product.php
+++ admin/product.php
@@ -218,6 +218,7 @@ $brands = get_all_brands_ctr($user_id);
     </form>
   </div>
 </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="../js/product.js"></script>
 </body>
 </html>
